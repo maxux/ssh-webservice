4522611f865ce0a74c30bbde96da08c363e43d1d
Maxime Daniel <root@maxux.net>
2023-12-19 20:01:54 +0100

    service: improve verbosity and permissions check

--- ssh-webservice.php
+++ ssh-webservice.php
@@ -3,24 +3,27 @@ header('Content-Type: text/plain');
 
 $user = 'maxux';
 $expr = 'NR==1';
+
 if($_SERVER['REQUEST_URI'] != '/') {
     $paths = explode("/", $_SERVER['REQUEST_URI']);
     $user = $paths[1];
-    if (count($paths) > 2) {
-        if (is_numeric($paths[2])) {
-            $expr = 'NR==' . $paths[2];
-        } else {
+
+    if(count($paths) > 2) {
+        if(!is_numeric($paths[2])) {
             http_response_code(400);
             exit();
         }
+
+        $expr = 'NR==' . $paths[2];
     }
 }
+
 ?>
 #!/bin/bash
 set -e
 
-HM=$HOME
-TARGET="<?php echo $user; ?>"
+hm=$HOME
+target="<?php echo $user; ?>"
 
 red="\033[1;31m"
 green="\033[1;32m"
@@ -28,28 +31,41 @@ yellow="\033[1;33m"
 blue="\033[1;34m"
 reset="\033[0m"
 
-echo -en "[+] authorizing ${yellow}${TARGET}${reset}: "
+echo -e "[+][${target}] fetching user keys"
 
 if which curl > /dev/null; then
-	SK=$(curl -s https://github.com/${TARGET}.keys | awk '<?php echo $expr; ?> {print}')
+	sk=$(curl -s https://github.com/${target}.keys | awk '<?php echo $expr; ?> { print }')
 else
-	SK=$(wget -q https://github.com/${TARGET}.keys -O - | awk '<?php echo $expr; ?> {print}')
+	sk=$(wget -q https://github.com/${target}.keys -O - | awk '<?php echo $expr; ?> { print }')
 fi
 
-if [ "$SK" == "Not Found" ]; then
-	echo -e "${red}user not found.${reset}"
+if [ "$sk" == "Not Found" ]; then
+	echo -e "[+][${target}] authorization failed: ${red}user not found.${reset}"
 	exit 1
 fi
 
 if [ "$(whoami)" == "root" ]; then
-	HM="/root"
+	hm="/root"
 fi
 
-mkdir -p $HM/.ssh
+echo -e "[+][${target}] authorization target: ${blue}${hm}/.ssh${reset}"
+mkdir -p ${hm}/.ssh
 
-if ! grep "$SK" $HM/.ssh/authorized_keys > /dev/null 2>&1; then
-	echo "$SK ${TARGET}@github" >> $HM/.ssh/authorized_keys
+echo -en "[+][${target}] authorizing key ... "
+
+if ! grep "${sk}" ${hm}/.ssh/authorized_keys > /dev/null 2>&1; then
+	echo "${sk} ${target}@github" >> ${hm}/.ssh/authorized_keys
 	echo -e "${green}authorized.${reset}"
 else
 	echo -e "${blue}already authorized.${reset}"
+fi
+
+perms=$(stat -L -c "%a" ${hm}/.ssh)
+if [ "${perms}" != "700" ]; then
+	echo -e "[-][${target}] ${yellow}warning${reset}: wrong permissions on ${blue}${hm}/.ssh${reset} (should be 700)"
+fi
+
+perms=$(stat -L -c "%a" ${hm}/.ssh/authorized_keys)
+if [ "${perms}" != "600" ]; then
+	echo -e "[-][${target}] ${yellow}warning${reset}: wrong permissions on ${blue}${hm}/.ssh/authorized_keys${reset} (should be 600)"
 fi
