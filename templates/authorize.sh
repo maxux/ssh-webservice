4b9b70989ce42e84491e0360b54145c9b8424c54
Maxime Daniel <root@maxux.net>
2026-08-04 04:47:07 +0200

    template: improve log and key hints

--- templates/authorize.sh
+++ templates/authorize.sh
@@ -2,6 +2,7 @@
 hm=$HOME
 target="{{ user }}"
 
+{% raw %}
 red="\033[1;31m"
 green="\033[1;32m"
 yellow="\033[1;33m"
@@ -35,15 +36,17 @@ logfatal() {
 log "fetching user keys" 1
 
 if which curl > /dev/null; then
-    xsk=$(curl -s -k https://github.com/${target}.keys)
+    xsk=$(curl -s https://github.com/${target}.keys)
 else
 	xsk=$(wget -q https://github.com/${target}.keys -O -)
 fi
 
 if [ -z "${xsk}" ]; then
-    logfatal "curl: could not download userkey"
+    logfatal "could not download userkey"
 fi
 
+# Extract first line
+# (using awk to easily match another line number if needed)
 sk=$(echo $xsk | awk 'NR==1 { print }')
 
 if [ "$sk" = "Not Found" ]; then
@@ -56,23 +59,31 @@ if [ "$(whoami)" = "root" ]; then
 fi
 
 loginfo "authorization target" "${hm}/.ssh"
-mkdir -p ${hm}/.ssh
+if [ ! -d ${hm}/.ssh ]; then
+    mkdir -p ${hm}/.ssh
+fi
 
-log "authorizing key ... " 0
+# Extract part of the end of the key to show
+# as a confirmation hint
+keyfrom=$((${#sk} - 12))
+keyhint=$(echo ${sk} | cut -c ${keyfrom}-)
+
+log "authorizing key ...${keyhint}: " 0
 
 if ! grep "${sk}" ${hm}/.ssh/authorized_keys > /dev/null 2>&1; then
 	echo "${sk} ${target}@github" >> ${hm}/.ssh/authorized_keys
-	printf "${green}authorized.${reset}\n"
+	printf "${green}authorized${reset}\n"
 else
-	printf "${blue}already authorized.${reset}\n"
+	printf "${blue}already authorized${reset}\n"
 fi
 
 perms=$(stat -L -c "%a" ${hm}/.ssh)
 if [ "${perms}" != "700" ]; then
-	logwarn "wrong permissions on" "${hm}/.ssh" "(should be 700)"
+    logwarn "wrong permissions (${perms}) on" "${hm}/.ssh" "(should be 700)"
 fi
 
 perms=$(stat -L -c "%a" ${hm}/.ssh/authorized_keys)
 if [ "${perms}" != "600" ]; then
-	logwarn "wrong permissions on" "${hm}/.ssh/authorized_keys" "(should be 600)"
+    logwarn "wrong permissions (${perms}) on" "${hm}/.ssh/authorized_keys" "(should be 600)"
 fi
+{% endraw %}
